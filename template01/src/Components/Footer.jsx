fac38501d4ceee0201250b845c641701791d97c1
Pablo <crundall@gmail.com>
2021-10-02 20:38:00 -0300

    first commit

---
+++ template01/src/Components/Footer.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <footer className="mt-auto py-3 container text-center">
+      &copy; Crundall Corp 2021
+    </footer>
+  );
+};
+
+export default Footer;
